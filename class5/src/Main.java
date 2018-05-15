dd318f1cda1ff676810b7f621f2a1c518be08620
張家明 <alexsender348979@gmail.com>
2018-05-15 16:45:24 +0800

    5/8 class

---
+++ class5/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        Mainframe frm=new Mainframe();
+        frame1 frm2 =new frame1();
+        frm.setVisible(true);
+        frm2.setVisible(true);
+    }
+}
